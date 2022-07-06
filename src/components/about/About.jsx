9d6a101c59f5b1bee5e63293ea749addb5a6c1b7
Leo Lee <leolee901115@gmail.com>
2022-07-06 15:55:40 -0700

    testing

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,14 @@ import './about.css'
 
 const About = () => {
   return (
-    <div><h1>About</h1></div>
+    <div>
+      <h1>
+        About
+      </h1>
+      <h2>
+        I am Leo Thl, a full-stack web developer.
+      </h2>
+    </div>
   )
 }
 

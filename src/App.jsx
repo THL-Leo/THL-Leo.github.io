cea3f549d95010c2b6e0f6fc2378c8ca529a197b
Leo Lee <leolee901115@gmail.com>
2022-07-06 15:50:55 -0700

    modularized components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import About from './components/about/About'
+import Projects from './components/projects/Projects'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <About />
+    </>
   )
 }
 
